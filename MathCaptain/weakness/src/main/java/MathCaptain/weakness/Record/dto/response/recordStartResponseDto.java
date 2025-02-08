9feec073bcc8a7c5392ac74c988b2e478a63c635
JayongTest <leean6652@google.com>
2025-02-09 02:15:52 +0900

    {refactor} 요구사항에 맞게 기록 시작시 해당 유저의 일간 목표 시간을 함께 반환

--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/Record/dto/response/recordStartResponseDto.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/Record/dto/response/recordStartResponseDto.java
@@ -1,4 +1,14 @@
 package MathCaptain.weakness.Record.dto.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class recordStartResponseDto {
+
+    private Long recordId;
+
+    private Long userDailyGoal;
+
 }
