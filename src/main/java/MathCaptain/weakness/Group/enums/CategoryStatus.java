e50dc867afa48fc561eda027d7bbfb554f51e2bb
JayongTest <leean6652@google.com>
2025-02-09 19:42:03 +0900

    {style} branch 정리

--- src/main/java/MathCaptain/weakness/Group/enums/CategoryStatus.java
+++
@@ -1,7 +0,0 @@
-package MathCaptain.weakness.Group.enums;
-
-public enum CategoryStatus {
-    FITNESS,
-    STUDY,
-    READING
-}
--- src/main/java/MathCaptain/weakness/Recruitment/enums/RecruitmentStatus.java
+++
@@ -1,7 +0,0 @@
-package MathCaptain.weakness.Recruitment.enums;
-
-public enum RecruitmentStatus {
-    // 모집중, 모집종료
-    RECRUITING,
-    END
-}
