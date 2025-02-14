ebbccaec26d609c02d7beb2542c7cf33cc1b5c1d
JayongLee <dlwpdyd201@naver.com>
2025-02-14 16:09:17 +0900

    {feat} 새로운 테스트 데이터 (그룹 모집글 / 댓글) 추가

--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/TestInit.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/TestInit.java
@@ -6,6 +6,11 @@ import MathCaptain.weakness.Group.enums.CategoryStatus;
 import MathCaptain.weakness.Group.enums.GroupRole;
 import MathCaptain.weakness.Group.repository.GroupRepository;
 import MathCaptain.weakness.Group.repository.RelationRepository;
+import MathCaptain.weakness.Recruitment.domain.Comment;
+import MathCaptain.weakness.Recruitment.domain.Recruitment;
+import MathCaptain.weakness.Recruitment.enums.RecruitmentStatus;
+import MathCaptain.weakness.Recruitment.repository.CommentRepository;
+import MathCaptain.weakness.Recruitment.repository.RecruitmentRepository;
 import MathCaptain.weakness.global.Security.jwt.JwtTestUtil;
 import MathCaptain.weakness.User.domain.Users;
 import MathCaptain.weakness.User.repository.UserRepository;
@@ -25,6 +30,8 @@ public class TestInit {
     private final PasswordEncoder passwordEncoder;
     private final GroupRepository groupRepository;
     private final RelationRepository relationRepository;
+    private final RecruitmentRepository recruitmentRepository;
+    private final CommentRepository commentRepository;
 
 
     @PostConstruct
@@ -32,15 +39,15 @@ public class TestInit {
     public void init() {
 
         String email1 = "dlwpdyd201@naver.com";
-        String refreshToken1 = JwtTestUtil.createTestJwt(email1);
+//        String refreshToken1 = JwtTestUtil.createTestJwt(email1);
 
         String email2 = "test2@example.com";
-        String refreshToken2 = JwtTestUtil.createTestJwt(email2);
+//        String refreshToken2 = JwtTestUtil.createTestJwt(email2);
 
         String email3 = "test";
-        String refreshToken3 = JwtTestUtil.createTestJwt(email3);
+//        String refreshToken3 = JwtTestUtil.createTestJwt(email3);
 
-        // 테스트 유저 생성
+        /// 테스트 유저 생성
         Users users1 = Users.builder()
                 .userId(1L)
                 .email(email1)
@@ -48,7 +55,7 @@ public class TestInit {
                 .name("tester01")
                 .nickname("tester01")
                 .phoneNumber("01012345678")
-                .refreshToken(refreshToken1)
+//                .refreshToken(refreshToken1)
                 .build();
 
         Users users2 = Users.builder()
@@ -58,7 +65,7 @@ public class TestInit {
                 .name("tester02")
                 .nickname("tester02")
                 .phoneNumber("01056781234")
-                .refreshToken(refreshToken2)
+//                .refreshToken(refreshToken2)
                 .build();
 
         Users users3 = Users.builder()
@@ -68,7 +75,7 @@ public class TestInit {
                 .name("tester")
                 .nickname("tester")
                 .phoneNumber("01011111111")
-                .refreshToken(refreshToken3)
+//                .refreshToken(refreshToken3)
                 .build();
 
 
@@ -80,7 +87,7 @@ public class TestInit {
         Users leader = userRepository.findByUserId(1L)
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
 
-//         테스트 그룹 생성
+        /// 테스트 그룹 생성
         Group group = Group.builder()
                 .leader(leader)
                 .name("testGroup")
@@ -106,28 +113,29 @@ public class TestInit {
 
         relationRepository.save(join);
 
-//        Recruitment recruitment = Recruitment.builder()
-//                .postId(1L)
-//                .author(leader)
-//                .recruitGroup(group)
-//                .title("테스트용 모집글")
-//                .content("테스트용 모집글입니다.")
-//                .category(CategoryStatus.STUDY)
-//                .build();
-//
-//        recruitmentRepository.save(recruitment);
-//        log.info("테스트 모집글 생성 완료");
-//
-//        Comment comment = Comment.builder()
-//                .author(leader)
-//                .post(recruitment)
-//                .content("테스트용 댓글입니다.")
-//                .build();
-//
-//        commentRepository.save(comment);
-//        log.info("테스트 댓글 생성 완료");
-//
-//        log.info("테스트 데이터 생성 완료");
+        /// 테스트 모집글 생성
+        Recruitment recruitment = Recruitment.builder()
+                .postId(1L)
+                .author(users1)
+                .recruitGroup(group)
+                .category(CategoryStatus.STUDY)
+                .title("testRecruitment")
+                .content("testContent")
+                .recruitmentStatus(RecruitmentStatus.RECRUITING)
+                .build();
 
+        recruitmentRepository.save(recruitment);
+        log.info("테스트 모집글 생성 완료");
+
+        /// 테스트 댓글 생성
+        Comment comment = Comment.builder()
+                .commentId(1L)
+                .author(users1)
+                .post(recruitment)
+                .content("testComment")
+                .build();
+
+        commentRepository.save(comment);
+        log.info("테스트 댓글 생성 완료");
     }
 }
