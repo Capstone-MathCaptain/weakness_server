3048668e1b0351d187ce6edb8d232d4e466a7444
Jeyong <dlwpdyd201@naver.com>
2025-03-29 13:32:01 +0900

    [refactor] DTO 네이밍 변경에 따른 수정

--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/controller/GroupController.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/controller/GroupController.java
@@ -1,15 +1,15 @@
 package MathCaptain.weakness.domain.Group.controller;
 
-import MathCaptain.weakness.domain.Group.dto.request.GroupSearchRequestDto;
-import MathCaptain.weakness.domain.Group.dto.request.GroupUpdateRequestDto;
-import MathCaptain.weakness.domain.Group.dto.response.GroupDetailResponseDto;
-import MathCaptain.weakness.domain.Group.dto.response.GroupMemberListResponseDto;
-import MathCaptain.weakness.domain.Group.dto.response.GroupResponseDto;
-import MathCaptain.weakness.domain.Group.dto.response.RelationResponseDto;
+import MathCaptain.weakness.domain.Group.dto.request.GroupSearchRequest;
+import MathCaptain.weakness.domain.Group.dto.request.GroupUpdateRequest;
+import MathCaptain.weakness.domain.Group.dto.response.GroupDetailResponse;
+import MathCaptain.weakness.domain.Group.dto.response.GroupMemberListResponse;
+import MathCaptain.weakness.domain.Group.dto.response.GroupResponse;
+import MathCaptain.weakness.domain.Group.dto.response.RelationResponse;
 import MathCaptain.weakness.domain.User.entity.Users;
 import MathCaptain.weakness.domain.User.dto.response.UserResponse;
 import MathCaptain.weakness.domain.Group.service.GroupService;
-import MathCaptain.weakness.domain.Group.dto.request.GroupCreateRequestDto;
+import MathCaptain.weakness.domain.Group.dto.request.GroupCreateRequest;
 import MathCaptain.weakness.domain.Group.service.RelationService;
 import MathCaptain.weakness.global.Api.ApiResponse;
 import MathCaptain.weakness.global.annotation.LoginUser;
@@ -32,27 +32,28 @@ public class GroupController {
 
     // 유저가 속한 그룹을 모두 보여줌
     @GetMapping("/group")
-    public ApiResponse<List<GroupResponseDto>> getUsersGroups(@LoginUser Users loginUser) {
+    public ApiResponse<List<GroupResponse>> getUsersGroups(@LoginUser Users loginUser) {
         return groupService.getUsersGroups(loginUser);
     }
 
     // 그룹 조회
     @GetMapping("/group/{groupId}")
-    public ApiResponse<GroupResponseDto> groupInfo(@PathVariable Long groupId) {
+    public ApiResponse<GroupResponse> groupInfo(@PathVariable Long groupId) {
         return ApiResponse.ok(groupService.getGroupInfo(groupId));
     }
 
     // 그룹 생성
     @PostMapping("/group")
-    public ApiResponse<GroupResponseDto> createGroup(@Valid @LoginUser Users loginUser,
-                                                     @RequestBody GroupCreateRequestDto groupCreateRequestDto, HttpServletResponse response) {
-        return groupService.createGroup(loginUser ,groupCreateRequestDto, response);
+    public ApiResponse<GroupResponse> createGroup(@Valid @LoginUser Users loginUser,
+                                                  @RequestBody GroupCreateRequest groupCreateRequest) {
+        return groupService.createGroup(loginUser , groupCreateRequest);
     }
 
     // 그룹 정보 수정
     @PutMapping("/group/{groupId}")
-    public ApiResponse<GroupResponseDto> updateGroup(@Valid @PathVariable Long groupId, @RequestBody GroupUpdateRequestDto groupUpdateRequestDto) {
-        return groupService.updateGroupInfo(groupId, groupUpdateRequestDto);
+    public ApiResponse<GroupResponse> updateGroup(@Valid @LoginUser Users loginUser,
+                                                  @PathVariable Long groupId, @RequestBody GroupUpdateRequest groupUpdateRequest) {
+        return groupService.updateGroupInfo(loginUser, groupId, groupUpdateRequest);
     }
 
     // 그룹 삭제
@@ -71,26 +72,26 @@ public class GroupController {
 
     // 그룹 관계 조회
     @GetMapping("/group/relation/{relationId}")
-    public RelationResponseDto relationInfo(@PathVariable Long relationId) {
-        return relationService.getRelationInfo(relationId);
+    public RelationResponse relationInfo(@PathVariable Long relationId) {
+        return relationService.getRelationResponse(relationId);
     }
 
     // 그룹 상세 정보
     @GetMapping("/group/detail/{groupId}")
-    public ApiResponse<GroupDetailResponseDto> groupDetail(@PathVariable Long groupId) {
+    public ApiResponse<GroupDetailResponse> groupDetail(@PathVariable Long groupId) {
         return groupService.getGroupDetail(groupId);
     }
 
     // 그룹 멤버 리스트 (그룹 상세 페이지)
     @GetMapping("/group/detail/{groupId}/members")
-    public ApiResponse<List<GroupMemberListResponseDto>> GroupMemberList(@PathVariable Long groupId) {
+    public ApiResponse<List<GroupMemberListResponse>> GroupMemberList(@PathVariable Long groupId) {
         return ApiResponse.ok(relationService.getGroupMemberList(groupId));
     }
 
     // 그룹 검색 (그룹명)
     @PostMapping("/group/search")
-    public ApiResponse<List<GroupResponseDto>> searchGroup(@RequestBody GroupSearchRequestDto groupSearchRequestDto) {
-        return groupService.getGroupInfo(groupSearchRequestDto.getGroupName());
+    public ApiResponse<List<GroupResponse>> searchGroup(@RequestBody GroupSearchRequest groupSearchRequest) {
+        return groupService.getGroupInfo(groupSearchRequest.getGroupName());
     }
 
     @GetMapping("/group/total")
