7b999f83d195cbfd77a84a6db7c33984edb836a7
Jeyong <dlwpdyd201@naver.com>
2025-04-13 18:37:54 +0900

    [feat] LLM용 DB 추가 및 해당 DB에 사용할 도메인 연결 / WebSocket 설정

--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/WeaknessApplication.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/WeaknessApplication.java
@@ -3,10 +3,12 @@ package MathCaptain.weakness;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@ComponentScan(basePackages = "MathCaptain")
 public class WeaknessApplication {
 
 	public static void main(String[] args) {
