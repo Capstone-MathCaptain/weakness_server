566b519013ede8d1ad7408645eb35fdca4449185
Jeyong <dlwpdyd201@naver.com>
2025-04-17 19:07:38 +0900

    [fix] enum 한글 매칭

--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/enums/CategoryStatus.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/enums/CategoryStatus.java
@@ -1,7 +1,31 @@
 package MathCaptain.weakness.domain.Group.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum CategoryStatus {
-    FITNESS,
-    STUDY,
-    RUNNING
+    FITNESS("헬스"),
+    STUDY("공부"),
+    RUNNING("러닝");
+
+    private final String value;
+
+    CategoryStatus(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static CategoryStatus fromValue(String value) {
+        for (CategoryStatus status : CategoryStatus.values()) {
+            if (status.value.equals(value) || status.name().equals(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("정의되지 않은 값 입니다 : " + value);
+    }
 }
--- MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/enums/RequestStatus.java
+++ MathCaptain/weakness/src/main/java/MathCaptain/weakness/domain/Group/enums/RequestStatus.java
@@ -1,8 +1,32 @@
 package MathCaptain.weakness.domain.Group.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RequestStatus {
-    WAITING,
-    ACCEPTED,
-    REJECTED,
-    CANCELED
+    WAITING("가입요청 중"),
+    ACCEPTED("가입요청 수락"),
+    REJECTED("가입요청 거절"),
+    CANCELED("가입요청 취소");
+
+    private final String value;
+
+    RequestStatus(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static RequestStatus fromValue(String value) {
+        for (RequestStatus status : RequestStatus.values()) {
+            if (status.value.equals(value) || status.name().equals(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("정의되지 않은 값 입니다 : " + value);
+    }
 }
